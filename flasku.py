334113653e049f5c734d2d1e774c0810c2f14f45
verysetiawan <kangphery@gmail.com>
2020-05-18 15:39:26 +0700

    commit awal

--- flasku.py
+++ flasku.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    nilai = 100
-    #perulangan
-    angka = [1,2,3,4,5,6,7,8,9,10]
-    return render_template("index.html", value=nilai, angka=angka)
+    return "hello world!!!"
 
 if __name__ == "__main__":
     app.run (host='0.0.0.0', debug=True)
